f0cf29ea49d28ba09f77dc5ba8cdefb8f752d567
said <saiddemirhan@gmail.com>
2021-11-08 21:36:02 +0100

    v3

--- src/test/java/com/techproed/day04/GetRequest01.java
+++ src/test/java/com/techproed/day04/GetRequest01.java
@@ -1,6 +1,8 @@
 package com.techproed.day04;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -29,11 +31,37 @@ public class GetRequest01 {
 
         Response response=given().accept("application/json").when().get(url);
 
+        response.prettyPrint();
+
         //4 actual result olusturuyoruz. (responce body ile)
 
+        //responcse body ile ilgili islem yapmayacagimiz icin simdi olusturmayacagiz.
 
         //5 dogrulama yapiyoruz. (assertion)
 
-        response.prettyPrint();
+        System.out.println("Status code: "+response.getStatusCode());
+        System.out.println("Content type: "+response.getContentType());
+        System.out.println("Status line: "+response.getStatusLine());
+        System.out.println("=========================");
+        System.out.println("Headers: "+response.getHeaders());
+        System.out.println("=========================");
+
+        //birinci yol
+
+        Assert.assertEquals(200,response.getStatusCode());
+
+        Assert.assertEquals("application/json; charset=utf-8",response.getContentType());
+
+        Assert.assertEquals("HTTP/1.1 200 OK",response.getStatusLine());
+
+        System.out.println("=========================");
+
+        //2. yol
+
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).//aceept ("application/json") da yazilabilir.
+                statusLine("HTTP/1.1 200 OK");
     }
 }
