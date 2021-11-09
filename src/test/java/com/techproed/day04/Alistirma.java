838b5a6204ad54c3fcd770fdb368dbe2caec33e4
said <saiddemirhan@gmail.com>
2021-11-09 22:11:07 +0100

    v5

--- src/test/java/com/techproed/day04/Alistirma.java
+++ src/test/java/com/techproed/day04/Alistirma.java
@@ -1,41 +1,39 @@
 package com.techproed.day04;
 
 import io.restassured.response.Response;
-import org.junit.Assert;
 import org.junit.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class Alistirma {
-    //        https://restful-booker.herokuapp.com/booking/10 url'ine bir GET
-    //        request gonderdigimizde donen Response'un,
-    //        status code'unun 200,
-    //        ve content type'inin application/json; charset=utf-8,
-    //        ve Server isimli Header'in degerinin Cowboy,
-    //        ve status Line'in HTTP/1.1 200 OK
-    //        ve response suresinin 5 sn'den kisa oldugunu manuel olarak test ediniz.
-
     @Test
-    public void test() throws InterruptedException {
-        String url ="https://restful-booker.herokuapp.com/booking/10";
+    public void get01(){
+        /*
+        https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request gonderdigimizde donen Response’un,
+        status code’unun 200,
+        ve content type’inin application/json; charset=utf-8,
+        ve Server isimli Header’in degerinin Cowboy,
+        ve status Line’in HTTP/1.1 200 OK
+        ve response suresinin 5 sn’den kisa oldugunu manuel olarak test ediniz.
+         */
 
-        Response response=given().accept("application/json").when().get(url);
+        // 1 - request url ve body'sini hazirlamak
 
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Expected Data'yi hazirla
+        // 3- Response'u kaydet
+
+        Response response = given().when().get(url);
         response.prettyPrint();
 
-        response.then().statusCode(200);
+        System.out.println("Status code : " + response.getStatusCode());  //Status code : 200
+        System.out.println("Content Type : " + response.getContentType()); //Content Type : application/json; charset=utf-8
+        System.out.println("Server header degeri : " + response.getHeader("Server")); //Server header degeri : Cowboy
+        System.out.println("Status Line : " + response.getStatusLine()); //Status Line : HTTP/1.1 200 OK
+        System.out.println("Response suresi : " + response.getTime()); //Response suresi : 2375
 
-        Assert.assertEquals("application/json; charset=utf-8",response.getContentType());
-
-        System.out.println(response.getHeader(url));
-
-        System.out.println("==============================");
-
-        Assert.assertEquals("Cowboy",response.getHeader("url"));
-
-        response.then().statusLine("HTTP/1.1 200 OK");
-
-        response.then().wait(5000);
 
     }
+
+
 }
